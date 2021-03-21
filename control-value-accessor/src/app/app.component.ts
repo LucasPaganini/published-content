b9a8680294b49c85fc25ef4ec571189ed4323539
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-03-21 10:33:17 -0300

    Use date input in root component

--- control-value-accessor/src/app/app.component.ts
+++ control-value-accessor/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'control-value-accessor';
+  public readonly dateControl = new FormControl(new Date());
+
+  public date = new Date();
+
+  public ngOnInit(): void {
+    setInterval(() => {
+      console.log(this.date);
+    }, 2000);
+  }
 }
--- control-value-accessor/src/app/app.module.ts
+++ control-value-accessor/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { DateInputModule } from './date-input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DateInputModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
